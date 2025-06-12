b21e542427cfa5600c6034fc2d8b07a08bc75338
Atharv Patil <115093211+atharv0004@users.noreply.github.com>
2025-06-12 14:06:33 +0530

    Updated IndiaMap.jsx

--- src/components/IndiaMap.jsx
+++ src/components/IndiaMap.jsx
@@ -43,7 +43,7 @@ const IndiaMap = () => {
     center={[22.9734, 78.6569]} // Center of India
     zoom={5.2}
     scrollWheelZoom={false} // 🔴 Disable scroll zoom
-    style={{ height: "80vh", width: "100%" }}
+    style={{ height: "85vh", width: "100%" }}
       >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
