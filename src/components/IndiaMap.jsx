84f97c419c4988dc3c6426cf7ae63636bf49d609
Atharv Patil <115093211+atharv0004@users.noreply.github.com>
2025-06-12 13:58:28 +0530

    Updated IndiaMap.jsx

--- src/components/IndiaMap.jsx
+++ src/components/IndiaMap.jsx
@@ -41,7 +41,7 @@ const IndiaMap = () => {
   return (
       <MapContainer
     center={[22.9734, 78.6569]} // Center of India
-    zoom={4}
+    zoom={6}
     scrollWheelZoom={false} // 🔴 Disable scroll zoom
     style={{ height: "100vh", width: "100%" }}
       >
