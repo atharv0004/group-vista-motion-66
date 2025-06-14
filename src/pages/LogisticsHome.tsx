ab405b3e813f3cb92f6068e832bc134e370bb500
Atharv Patil <115093211+atharv0004@users.noreply.github.com>
2025-06-14 23:14:39 +0530

    Updated LogisticsHome.tsx

--- src/pages/LogisticsHome.tsx
+++ src/pages/LogisticsHome.tsx
@@ -108,7 +108,7 @@ const LogisticsHome = () => {
       'Real-time visibility of inventory'
     ],
     icon: '🏬',
-    image: 'https://images.unsplash.com/photo-1566576912321-d58ddd7a6088?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80'
+    image: 'https://plus.unsplash.com/premium_photo-1749149653926-b6942cd3db56?q=80&w=1955&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   },
   {
     title: 'Last Mile Delivery',
