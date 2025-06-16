f098c6b32630b85baa4ab5ef7ebf278eb16c1942
Atharv Patil <115093211+atharv0004@users.noreply.github.com>
2025-06-16 13:04:12 +0530

    Updated EnterprisesHome.tsx

--- src/pages/EnterprisesHome.tsx
+++ src/pages/EnterprisesHome.tsx
@@ -136,7 +136,7 @@ const features = [
       'Planning for future Pan India market expansion',
     ],
     icon: '🛒',
-    image: 'https://images.unsplash.com/photo-1743416982545-eb6159f9746f?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    image: 'https://res.cloudinary.com/dxqsygmg1/image/upload/v1750058815/onion_fxuk2v.avif',
   },
 ];
 
