0bba1ccafa8709d10117aca2f5c0b2aeadd02564
Atharv Patil <115093211+atharv0004@users.noreply.github.com>
2025-06-16 21:07:06 +0530

    Updated LogisticsHome.tsx

--- src/pages/LogisticsHome.tsx
+++ src/pages/LogisticsHome.tsx
@@ -120,7 +120,7 @@ const LogisticsHome = () => {
   ],
   icon: '✈️',
   image: 'https://images.unsplash.com/photo-1587293852726-70cdb56c2866?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-}
+},
 
   {
     title: 'Sea Freight & Global Logistics',
