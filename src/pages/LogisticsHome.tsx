6df80eb67c83c3083afe5dea9d590c31625a1c03
Atharv Patil <115093211+atharv0004@users.noreply.github.com>
2025-06-17 15:20:06 +0530

    Updated LogisticsHome.tsx

--- src/pages/LogisticsHome.tsx
+++ src/pages/LogisticsHome.tsx
@@ -149,19 +149,7 @@ const LogisticsHome = () => {
     icon: '📡',
     image: 'https://plus.unsplash.com/premium_photo-1681487855134-d6c0434f91f8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   },
-  {
-    title: 'Consulting Services',
-    description: 'Expert guidance in supply chain, transportation strategy, and process improvement.',
-    features: [
-      'Cost reduction analysis',
-      'Logistics strategy development',
-      'Network design optimization',
-      'Process audits and SOP building',
-      'Change management and training'
-    ],
-    icon: '📊',
-    image: 'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80'
-  }
+  
 ];
 
   return (
